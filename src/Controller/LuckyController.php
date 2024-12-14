3bb8991ebd000a6774fb73807061f955374c6c2f
smart715 <smartmanage715@gmail.com>
2024-12-14 12:49:50 -0300

    controller view test

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,23 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+// Import necessary classes
+use Symfony\Component\HttpFoundation\Response; // Import this class
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number", name="app_lucky_number")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
